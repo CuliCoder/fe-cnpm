e002b27dfdc913900c8bf1fecfbe47c8f0247270
trungnc113 <nguyencongtrung13@gmail.com>
2024-10-10 12:20:45 +0700

    repair provider position

--- src/App.js
+++ src/App.js
@@ -16,113 +16,127 @@ import Products from "./components/Products/Products.jsx";
 import Payment from "./components/Checkout/Checkout.jsx";
 import SearchProduct from "./components/SearchProduct/SearchProduct.jsx";
 import Product_category from "./components/Product_category/Product_category.jsx";
+import { useEffect, useState } from "react";
+import { check_status } from "./Slice/status";
+import { useDispatch, useSelector } from "react-redux";
+import Spinner from "./components/Spinner/Spinner";
+import { fetchAllProducts } from "./Slice/products";
+import { fetchAllCategory } from "./Slice/categorySlice";
 function App() {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.status);
+  const products_page = useSelector((state) => state.products.products_page);
+  const category = useSelector((state) => state.category);
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+    dispatch(fetchAllCategory());
+    dispatch(check_status());
+  }, []);
+  
+  if (status.error === null) {
+    return <Spinner />;
+  }
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Mainpage />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/cart"
-            element={
-              <Layout>
-                <Cart />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/products"
-            element={
-              <Layout>
-                <Products />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={
-              <Layout>
-                <Login />
-              </Layout>
-            }
-          ></Route>
+    <Router>
+      {(products_page.loading || category.loading) && <Spinner />}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Mainpage />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/cart"
+          element={
+            <Layout>
+              <Cart />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/products"
+          element={
+            <Layout>
+              <Products />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={
+            <Layout>{status.error === 0 ? <MyAccount /> : <Login />}</Layout>
+          }
+        ></Route>
 
-          <Route
-            path="/register"
-            element={
-              <Layout>
-                <Register />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/product/:productId"
-            element={
-              <Layout>
-                <ContentProduct />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/search/:valueSearch"
-            element={
-              <Layout>
-                <SearchProduct />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/my-account"
-            element={
-              <Layout>
-                <MyAccount />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/checkout"
-            element={
-              <Layout>
-                <Payment />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/new-product"
-            element={
-              <Layout>
-                <newProduct />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/recover-password"
-            element={
-              <Layout>
-                <ForgotPassword />
-              </Layout>
-            }
-          ></Route>
-          <Route
-            path="/products/category/:category_id"
-            element={
-              <Layout>
-                <Product_category />
-              </Layout>
-            }
-          ></Route>
-          <Route path="*" element={<h1>404 Not Found</h1>}></Route>
-        </Routes>
-      </Router>
-    </Provider>
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/product/:productId"
+          element={
+            <Layout>
+              <ContentProduct />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/search/:valueSearch"
+          element={
+            <Layout>
+              <SearchProduct />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/my-account"
+          element={
+            <Layout>{status.error === 0 ? <MyAccount /> : <Login />}</Layout>
+          }
+        ></Route>
+        <Route
+          path="/checkout"
+          element={
+            <Layout>
+              <Payment />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/new-product"
+          element={
+            <Layout>
+              <newProduct />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/recover-password"
+          element={
+            <Layout>
+              <ForgotPassword />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/products/category/:category_id"
+          element={
+            <Layout>
+              <Product_category />
+            </Layout>
+          }
+        ></Route>
+        <Route path="*" element={<h1>404 Not Found</h1>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default React.memo(App);
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,32 +10,11 @@ import { fetchAllCategory } from "../../Slice/categorySlice";
 import Mainpage from "../Mainpage/Mainpage";
 import Login from "../Login/Login";
 export default function Layout({ children }) {
-  const dispatch = useDispatch();
-  const status = useSelector((state) => state.status);
-  const products_page = useSelector((state) => state.products.products_page);
-  const category = useSelector((state) => state.category);
-  const [isLogin, setIsLogin] = useState(null);
-  useEffect(() => {
-    dispatch(fetchAllProducts());
-    dispatch(fetchAllCategory());
-    dispatch(check_status());
-  }, []);
-  useEffect(() => {
-    if (status.error === 0) {
-      setIsLogin(true);
-    } else {
-      setIsLogin(false);
-    }
-  }, [status.error]);
-  if (isLogin === null) {
-    return <Spinner />;
-  }
   return (
     <div>
-      {(products_page.loading || category.loading) && <Spinner />}
       <Header />
       <main>
-        {children.type.name === "Login" ? (
+        {/* {children.type.name === "Login" ? (
           isLogin ? (
             <Mainpage />
           ) : (
@@ -45,7 +24,8 @@ export default function Layout({ children }) {
           <Login />
         ) : (
           children
-        )}
+        )} */}
+        {children}
       </main>
       <Footer />
     </div>
