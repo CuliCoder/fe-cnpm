7593f93e682435da0e49db665df56d6c6cf466f6
trungnc113 <nguyencongtrung13@gmail.com>
2024-11-27 16:45:49 +0700

    nothing

--- src/config/configAxios.js
+++ src/config/configAxios.js
@@ -18,9 +18,7 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
-    "Access-control-allow-origin": process.env.REACT_APP_API_URL,
-    "Access-Control-Allow-Credentials": true,
+    Accept: "application/json",
   },
 });
 
